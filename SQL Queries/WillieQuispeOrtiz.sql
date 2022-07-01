23b3a034f5646ef7d49b64bfd70a943e2f1de427
WillieQuispeOrtiz <85749389+WillieQuispeOrtiz@users.noreply.github.com>
2022-07-01 13:52:03 -0500

    Update WillieQuispeOrtiz.sql

--- SQL Queries/WillieQuispeOrtiz.sql
+++ SQL Queries/WillieQuispeOrtiz.sql
@@ -1,3 +1,5 @@
+/* 1. Retorna una tabla con las notas superiores a 17 con los nombres, seccion, curso y nota, para asi poder tener conocimiento de los alumnos sobresalientes 
+y brindarles apoyo para su ingreso a la universidad*/
 SELECT FirstName, ClassName, SubjectName, Score
 FROM Student S
 JOIN Grades G ON S.GradesId = G.GradesId
@@ -7,6 +9,8 @@ WHERE SCORE>17
 GROUP BY FirstName, ClassName, SubjectName, Score
 ORDER BY Score DESC;
 
+/*2. Retorna una tabla con las notas inferiores a 13 con los nombres, seccion, curso y nota, para asi poder darles apoyo creando un salon de nivelacion, 
+y asi tengan un mejor rendimiento */
 SELECT FirstName, ClassName, SubjectName, Score
 FROM Student S
 JOIN Grades G ON S.GradesId = G.GradesId
@@ -16,6 +20,8 @@ WHERE SCORE<13
 GROUP BY FirstName, ClassName, SubjectName, Score
 ORDER BY Score ASC;
 
+/*3. Retorna una tabla con los cursos, nombres de profesor y promedio de notas ordenados desde el curso con menor nota al curso con mejor nota, con esto
+podremos saber que cursos estan teniendo un bajo rendimiento e investigar si se trata de la enseñanza del profesor o el curso es muy exigente*/
 SELECT SubjectName, FirstName, AVG(Score)
 From Subject SJ
 JOIN Grades G ON SJ.SubjectId= G.SubjectId
@@ -23,8 +29,12 @@ JOIN Teacher T ON SJ.TeacherId=T.TeacherId
 GROUP BY SubjectName, FirstName
 ORDER BY AVG(Score) ASC;
 
-SELECT FirstName, GradeTypeName, Score
-From Student S
+/*4. Retorna una tabla con los nombres, tipo de nota, y nota de la seccion A que sean Trabajos Finales, para pasarlas al registro de notas de una forma
+mas sencilla*/
+SELECT ClassName, FirstName, GradeTypeName, Score
+From Class C
+JOIN Student S ON C.ClassId=S.ClassId
 JOIN Grades G ON S.GradesId= G.GradesId
 JOIN GradeType GT ON G.GradeTypeId=GT.GradeTypeId
-GROUP BY FirstName, GradeTypeName,Score;
+WHERE ClassName="A" && GradeTypeName="TF"
+GROUP BY ClassName, FirstName, GradeTypeName,Score;
